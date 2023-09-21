f9fc4fd9dded2ea1e5d5daebe02db41aeccfb8ad
Navtej Riar <nriar3@gatech.edu>
2023-09-20 18:35:16 -0700

    Created basic calculator functionality

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# Define a function to add two numbers
+def add_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+# Define a function to subtract two numbers
+def sub_numbers(num1, num2):
+    result = num1 - num2
+    return result
+
+# Define a function to multiply two numbers
+def mul_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+# Define a function to multiply two numbers
+def div_numbers(num1, num2):
+    result = num1 / num2
+    return result
